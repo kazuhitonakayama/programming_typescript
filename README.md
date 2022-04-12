c4dd285d18d9fec551104bc4d351afc95ff90098
kazuhitonakayama <firstjin19990101@gmail.com>
2022-04-12 21:44:56 +0900

    READMEにスクリプトを登録

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 ## Overview
 This repository is learing TypeScript with reading 『programming TypeScript』
+
+## Script
+### TSC（TypeScript Compile） and execute eslint
+
+```
+> yarn compile
+```
+
+### プログラムの実行
+
+```
+> yarn execute
+```
+
+## ESlintの実行
+
+```
+# lintの起動（Fixはしない）
+> yarn lint:eslint
+
+# lintの起動（Fixする）
+> yarn fix:eslint
+```
+
+
--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,11 @@
 "use strict";
-const a = 1 + 2;
-const b = a + 3;
-const c = {
-    apple: a,
-    banana: b,
-};
-const d = c.apple * 4;
-console.log(d);
+const a = 1 + "2";
+console.log(a);
+// const b = a + 3;
+// const c = {
+//     apple: a,
+//     banana: b,
+// };
+// const d = c.apple * 4;
+// console.log(d);
 // # sourceMappingURL=index.js.map
